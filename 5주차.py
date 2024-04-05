a216bb63308485327f09c01ddf3782bac937885d
LEESEONGUK1 <lsw6500@gmail.com>
2024-04-05 15:51:10 +0900

    Create 5주차.py
    5주차

---
+++ 5주차.py
@@ -0,0 +1,13 @@
+Num = input("점수를 입력하세요:")
+
+if (int(Num)) > 71 :
+    print("학점은 A 입니다.")
+
+elif (int(Num)) >= 41 :
+    print("학점은 B 입니다.")
+
+elif (int(Num)) >= 11 :
+    print("학점은 C 입니다.")
+
+elif (int(Num)) <= 10 :
+    print("학점은 D 입니다.")
