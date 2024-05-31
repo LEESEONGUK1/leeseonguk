69699c641e8863e2e5382ffd99210f9a6cc48255
LEESEONGUK1 <lsw6500@gmail.com>
2024-05-31 15:16:15 +0900

    13주차

--- 13주차.py
+++ 13주차.py
@@ -1,24 +1,28 @@
 class Beverage:
-    def __init__(self, name, price):
+    def __init__(self, name, count):
+        self.menu = {"커피": 3000, "녹차": 2500, "아이스티": 3000}
         self.name = name
-        self.price = price
+        self.count = count
+        self.total = 0
 
-    def calculate(self, quantity):
-        total_price = self.price * quantity
-        return total_price
+    def sell(self):
+        if self.name in self.menu:
+            self.total = self.menu[self.name] * self.count
+        else:
+            return "음료 이름이 잘못되었습니다. 다시 입력해 주세요."
 
-menu = {
-    "커피": 3000,
-    "녹차": 2500,
-    "아이스티": 3000}
 
-select = input("주문할 음료를 선택하세요 (커피, 녹차, 아이스티): ")
-quantity = int(input("수량을 입력하세요: "))
+def main():
+    choice_beverage = input("주문할 음료를 선택해 주세요(커피/녹차/아이스티): ")
+    choice_count = int(input("주문할 음료의 개수를 입력해 주세요: "))
+    order = Beverage(choice_beverage, choice_count)
 
-if select in menu:
-    beverage = Beverage(select, menu[select])
-    total_price = beverage.calculate(quantity)
-    print(f"총 가격은 {total_price}원 입니다.")
+    error_message = order.sell()
+    if error_message:
+        print(error_message)
+    else:
+        print(f"총 가격은 {order.total}원입니다.")
 
-else:
-    print("없는 메뉴 입니다.")
+
+if __name__ == "__main__":
+    main()
